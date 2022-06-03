313d4e08531bab3a53a4c3517f4ffdfc43be6d63
Agustín Covarrubias <gh@agucova.dev>
2022-06-03 07:37:06 +0000

    feat: add support for meilisearch

--- reguleque_search/upload_ts.py
+++ reguleque_search/upload_ts.py
@@ -1,20 +1,21 @@
 import os
 from pathlib import Path
-from time import sleep
-from typing import List
-from tqdm import tqdm
-from tqdm.utils import CallbackIOWrapper
+from typing import List, Union
 
 import requests
 import typer
-import typesense as ts
 from art import tprint
 from dask import dataframe as dd
-from dask.distributed import Client, progress
-from tenacity import retry, wait_fixed, stop_after_attempt, retry_if_result, retry_if_exception_type, RetryError
+from distributed.client import Client
+from tenacity import retry
+from tenacity.retry import retry_if_exception_type, retry_if_result
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_fixed
+from tqdm import tqdm
+from tqdm.utils import CallbackIOWrapper
 
 endpoint = "0.0.0.0"
-collection_name = "revenue_entry"
+collection_name = "reguleque"
 in_path = Path("data/in")
 warn_error_tolerance = 0.05
 
@@ -93,7 +94,7 @@ COLUMN_TYPES = {
     "observaciones": str,
     "enlace": str,
     "viáticos": str,
-    "pago_mensual": str
+    "pago_mensual": str,
 }
 
 REVENUE_SCHEMA = {
@@ -137,25 +138,22 @@ LOG_PROMPT = typer.style("[PROMPT]", fg=typer.colors.WHITE, bold=True)
 # @dask.delayed
 def load_file(filepath: Path) -> dd.DataFrame:
     columns = HONORARIOS_COLUMNS if "honorarios" in str(filepath) else COLUMNS
-    entries = (
-        dd.read_csv(
-            filepath,
-            sep=";",
-            names=columns,
-            dtype=COLUMN_TYPES,
-            header=None,
-            skip_blank_lines=True,
-            na_filter=True,
-            na_values="Indefinido",
-            encoding="ISO-8859-1",
-            # index_col="id",
-        )
-        .dropna(how="all")
-    )
+    entries = dd.read_csv(
+        filepath,
+        sep=";",
+        names=columns,
+        dtype=COLUMN_TYPES,
+        header=None,
+        skip_blank_lines=True,
+        na_filter=True,
+        na_values="Indefinido",
+        encoding="ISO-8859-1",
+        # index_col="id",
+    ).dropna(how="all")
     for col in entries:
-        #get dtype for column
-        dt = entries[col].dtype 
-        #check if it is a number
+        # get dtype for column
+        dt = entries[col].dtype
+        # check if it is a number
         if dt == int or dt == float:
             entries[col] = entries[col].fillna(0)
         else:
@@ -163,19 +161,32 @@ def load_file(filepath: Path) -> dd.DataFrame:
 
     return entries
 
+
 def non_200(r):
     return r.status_code != 200
 
-@retry(retry=retry_if_result(non_200) | retry_if_exception_type(requests.exceptions.ConnectionError), stop=stop_after_attempt(2), wait=wait_fixed(3), reraise=True)
+
+@retry(
+    retry=retry_if_result(non_200)
+    | retry_if_exception_type(requests.exceptions.ConnectionError),
+    stop=stop_after_attempt(2),
+    wait=wait_fixed(3),
+    reraise=True,
+)
 def import_chunk(chunk, api_key, endpoint, session, port=443, protocol="https"):
     file_size = os.stat(chunk).st_size
     with open(chunk, "rb") as f:
         with tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024) as t:
             wrapped_file = CallbackIOWrapper(t.update, f, "read")
-            return session.post(f"{protocol}://{endpoint}:{port}/collections/{collection_name}/documents/import?action=upsert", data=wrapped_file, headers={
-                            "X-TYPESENSE-API-KEY": api_key,
-                            }, stream=True)
-                        
+            return session.post(
+                f"{protocol}://{endpoint}:{port}/collections/{collection_name}/documents/import?action=upsert",
+                data=wrapped_file,
+                headers={
+                    "X-TYPESENSE-API-KEY": api_key,
+                },
+                stream=True,
+            )
+
 
 def main(
     endpoint: str = endpoint,
@@ -183,9 +194,9 @@ def main(
     protocol: str = "http",
     collection_name: str = collection_name,
     in_path: Path = in_path,
-    api_key: str = os.getenv("TYPESENSE_API_KEY"),
+    api_key: Union[str, None] = os.getenv("TYPESENSE_API_KEY"),
     drop: bool = False,
-    skip_conversion: bool = False
+    skip_conversion: bool = False,
 ):
     tprint("Reguleque")
     tsClient = ts.Client(
@@ -201,7 +212,7 @@ def main(
             ],
             "retry_interval_seconds": 15,
             "connection_timeout_seconds": 90,
-            "num_retries": 10
+            "num_retries": 10,
         }
     )
     typer.echo(LOG_INFO + f" Connection to Typesense at {protocol}://{endpoint}:{port}")
@@ -233,7 +244,7 @@ def main(
             try:
                 tsClient.collections[collection_name].delete()
             except Exception:
-                pass 
+                pass
             # Create collection with the manual schema
             tsClient.collections.create(REVENUE_SCHEMA)
             typer.secho(LOG_INFO + " Created new schema.")
@@ -246,9 +257,14 @@ def main(
                 intermediate_path = Path(f"data/conversion/{name}")
                 typer.secho(LOG_INFO + f" Converting {name} to JSONL...")
                 if "honorarios" in name:
-                    typer.secho(LOG_INFO + f" Honorarios detected, special column schema enabled.")
-                dd.DataFrame.to_json(entries, intermediate_path, encoding="utf-8", lines=True)
-        
+                    typer.secho(
+                        LOG_INFO
+                        + f" Honorarios detected, special column schema enabled."
+                    )
+                dd.DataFrame.to_json(
+                    entries, intermediate_path, encoding="utf-8", lines=True
+                )
+
         session = requests.Session()
         for filepath in filepaths:
             name = Path(filepath).name
@@ -258,24 +274,37 @@ def main(
             typer.secho(LOG_INFO + f" Using {len(chunks)} chunks...")
             for chunk in chunks:
                 try:
-                    response = import_chunk(chunk, api_key, endpoint, session, port=port, protocol=protocol)
+                    response = import_chunk(
+                        chunk, api_key, endpoint, session, port=port, protocol=protocol
+                    )
                 except requests.exceptions.ConnectionError as e:
-                    typer.echo(LOG_ERR + f" Couldn't establish connection after repeated requests:", err=True)
+                    typer.echo(
+                        LOG_ERR
+                        + f" Couldn't establish connection after repeated requests:",
+                        err=True,
+                    )
                     print("   ", e)
                     raise typer.Abort()
-                    
+
                 if response.status_code != 200:
-                    typer.echo(LOG_ERR + f" Got response code {response.status_code}, with:", err=True)
+                    typer.echo(
+                        LOG_ERR + f" Got response code {response.status_code}, with:",
+                        err=True,
+                    )
                     print(response.text)
                     raise typer.Abort()
 
                 entries = response.text.split("\n")
-                errors = [line for line in entries if line != "{\"success\":true}"]
-                error_ratio = len(errors)/len(entries)
+                errors = [line for line in entries if line != '{"success":true}']
+                error_ratio = len(errors) / len(entries)
                 if error_ratio >= warn_error_tolerance:
-                    typer.echo(LOG_WARN + f" {round(error_ratio * 100, 1)}% of entries had errors. Sample:", err=True)
+                    typer.echo(
+                        LOG_WARN
+                        + f" {round(error_ratio * 100, 1)}% of entries had errors. Sample:",
+                        err=True,
+                    )
                     print(errors[1])
-                
+
             typer.secho(LOG_INFO + f" All chunks uploaded.")
 
     except ts.exceptions.RequestUnauthorized:
